ee566f6096f972b93e04a065c74d44bd303c315d
joey! <joeygonz111@outlook.com>
2021-06-23 17:51:19 -0700

    Missing header file

--- Shaders.h
+++ Shaders.h
@@ -2,11 +2,15 @@
 #define SHADERS_H
 
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <cerrno>
 #include <memory>
+
+std::string getFileContents(const char* fileName);
+
 
 #endif // ! SHADERS_H
